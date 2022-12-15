3098cfd1e64213979ba065b0317028cf4f94d2d5
Micha Alt <mail@lean-stack.de>
2022-12-15 22:36:34 +0100

    Set basic cores headers.

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:resource*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
+          { key: 'Access-Control-Max-Age', value: '86400' },
+        ],
+      },
+    ];
+  },
+}
+
+export default nextConfig;
+
