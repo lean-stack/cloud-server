ab0f67de76228705ae9ec7a4134b8ef181d1e9d2
Micha Buchholz <mail@lean-stack.de>
2023-05-14 20:29:23 +0200

    fix: don't send headers twice

--- middleware/cors-preflight.ts
+++ middleware/cors-preflight.ts
@@ -1,6 +1,5 @@
 export default defineEventHandler((event) => {
   if (event.path.startsWith('/api/') && isPreflightRequest(event)) {
-    appendCorsPreflightHeaders(event, { methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'] });
     sendNoContent(event);
   }
 });
